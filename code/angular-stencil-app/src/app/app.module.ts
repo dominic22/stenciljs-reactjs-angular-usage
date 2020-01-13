3122c50460837e9d5854d89799acb943db663dd0
dominic22 <nexus.domi123@gmail.com>
2020-01-13 22:05:23 +0100

    Add stencil to angular project

--- code/angular-stencil-app/src/app/app.module.ts
+++ code/angular-stencil-app/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
+            declarations: [
+              AppComponent
+            ],
+            imports: [
+              BrowserModule
+            ],
+            providers: [],
+            bootstrap: [AppComponent],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+          })
+export class AppModule {
+}
