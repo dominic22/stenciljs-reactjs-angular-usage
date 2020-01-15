693aca280b2ed1f76d1032a8c11b460be6ac3a49
dominic22 <nexus.domi123@gmail.com>
2020-01-15 13:17:03 +0100

    reformat code

--- code/angular-stencil-app/src/app/app.component.ts
+++ code/angular-stencil-app/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 
-@Component({
-             selector: 'app-root',
-             templateUrl: './app.component.html',
-             styleUrls: ['./app.component.less']
-           })
+@Component(
+  {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less']
+  })
 export class AppComponent {
   @ViewChild('popUp', { static: false })
   popUpRef: ElementRef<HTMLStPopUpElement>;
@@ -12,6 +13,7 @@ export class AppComponent {
   proButtonClick($event) {
     this.popUpRef.nativeElement.show();
   }
+
   onPopUpClose($event) {
     console.log('POP UP CLOSES', $event.detail);
   }
